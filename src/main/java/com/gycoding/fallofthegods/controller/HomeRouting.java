f39dc3ff74e2b27eda234710816e6b4c0ebf22e8
srtoxyc <ivi1000vm@gmail.com>
2024-03-14 16:48:21 +0100

    Documentation almost finished.

--- src/main/java/com/gycoding/fallofthegods/controller/HomeRouting.java
+++ src/main/java/com/gycoding/fallofthegods/controller/HomeRouting.java
@@ -1,12 +1,27 @@
 package com.gycoding.fallofthegods.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gycoding.fallofthegods.model.entities.ServerStatus;
 
 @RestController
 public class HomeRouting {
     @GetMapping("/")
     public String home() {
-        return "<h1 style=\"color: purple\">Developed by GY Coding.</h1>";
+        Resource resource = new ClassPathResource("static/index.html");
+
+        try {
+            return new String(Files.readAllBytes(Paths.get(resource.getURI())));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ServerStatus.NOT_FOUND.toString();
+        }
     }
 }
