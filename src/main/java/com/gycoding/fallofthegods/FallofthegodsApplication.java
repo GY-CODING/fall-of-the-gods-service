43683ec51edecd50526587d107bfd1cfac27da51
srtoxyc <ivi1000vm@gmail.com>
2024-05-21 11:17:37 +0200

    chore: add environment variables.

--- src/main/java/com/gycoding/fallofthegods/FallofthegodsApplication.java
+++ src/main/java/com/gycoding/fallofthegods/FallofthegodsApplication.java
@@ -1,11 +1,14 @@
 package com.gycoding.fallofthegods;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FallofthegodsApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(FallofthegodsApplication.class, args);
 	}
 }
