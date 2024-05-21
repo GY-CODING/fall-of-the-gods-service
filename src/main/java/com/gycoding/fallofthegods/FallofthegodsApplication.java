296fe7061ad86d3adcb617bbdd0a7178c3211a6e
ToxYc <96318474+gy-toxyc@users.noreply.github.com>
2024-05-21 11:41:59 +0200

    Develop (#28)
    * feat: better exception management (#15)
    
    * feat: add new exception management.
    
    * docs: update docs.
    
    * fix: update pom version.
    
    fix: optional management in some services procedures
    
    * styles: better height for changelog.
    
    * fix: add new exception management to old serverstatus-dependant endpoints.
    
    * joke: i'm a teapot!
    
    * feat: add detailed icon to every world.
    
    fix: fix every url of world images.
    Signed-off-by: srtoxyc <ivi1000vm@gmail.com>
    
    * feat: add detailed icon to every world.
    
    Signed-off-by: srtoxyc <ivi1000vm@gmail.com>
    
    * fix: up to date with GY Accounts v2.
    
    * chore: add environment variables.
    
    * fix: fix environment variables management.
    
    ---------
    
    Signed-off-by: srtoxyc <ivi1000vm@gmail.com>
    Co-authored-by: GYCODING <gycoding05@gmail.com>

--- src/main/java/com/gycoding/fallofthegods/FallofthegodsApplication.java
+++ src/main/java/com/gycoding/fallofthegods/FallofthegodsApplication.java
@@ -7,8 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FallofthegodsApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-		SpringApplication.run(FallofthegodsApplication.class, args);
+		try {
+			Dotenv dotenv = Dotenv.load();
+			dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		} catch(Exception e) {
+			System.out.println("Error loading .env file.");
+		} finally {
+			SpringApplication.run(FallofthegodsApplication.class, args);
+		}
 	}
 }
