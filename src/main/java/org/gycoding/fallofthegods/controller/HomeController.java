481609ddbabe3adb2a05c00c743d93c97c0a9172
srtoxyc <ivi1000vm@gmail.com>
2024-08-20 11:40:30 +0200

    fix: removed old docs.

--- src/main/java/org/gycoding/fallofthegods/controller/HomeController.java
+++ src/main/java/org/gycoding/fallofthegods/controller/HomeController.java
@@ -2,32 +2,11 @@ package org.gycoding.fallofthegods.controller;
 
 import org.gycoding.fallofthegods.model.entities.exceptions.FOTGAPIError;
 import org.gycoding.exceptions.model.APIException;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 @RestController
 public class HomeController {
-    @GetMapping("/")
-    public String home() throws APIException {
-        Resource resource = new ClassPathResource("static/index.html");
-
-        try {
-            return new String(Files.readAllBytes(Paths.get(resource.getURI())));
-        } catch (IOException e) {
-            throw new APIException(
-                    FOTGAPIError.HOME_NOT_FOUND.getCode(),
-                    FOTGAPIError.HOME_NOT_FOUND.getMessage(),
-                    FOTGAPIError.HOME_NOT_FOUND.getStatus()
-            );
-        }
-    }
-
     @GetMapping("/makemeacoffee")
     public String makeCoffee() throws APIException {
         throw new APIException(
