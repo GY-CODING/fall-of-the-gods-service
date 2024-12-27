48fd274bb56d22f0f20274d0d3e11bc9ddeea67e
ToxYc <96318474+gy-toxyc@users.noreply.github.com>
2024-12-27 19:40:51 +0100

    feat: limit access for some origins (#55)
    * feat: limit access to specific origins based on env
    
    * chore: update pom version

--- src/main/java/org/gycoding/fallofthegods/config/CORSConfig.java
+++ src/main/java/org/gycoding/fallofthegods/config/CORSConfig.java
@@ -11,7 +11,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @SuppressWarnings("null")
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("https://api.gateway.com", "http://localhost")
                 .allowedMethods(HttpMethod.GET.name())
                 .allowedHeaders("*")
                 .allowCredentials(true)
